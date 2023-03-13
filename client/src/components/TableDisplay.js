9f54d24c1ea1788952882375332ff8db699128c7
Piotr Szamocki <szamocki.piotr04@gmail.com>
2023-03-13 14:30:42 +0100

    Added displaying of data from synthetic DB. Added selectable fields wit data from synthetic DB.

--- client/src/components/TableDisplay.js
+++ client/src/components/TableDisplay.js
@@ -1,24 +1,88 @@
+import React from 'react';
 import TableRow from './TableRow';
 import '../styles/TableDisplay.css';
 
-function TableDisplay(props){
-    const headerRowData = Object.keys(props.rows[0]);
+class TableDisplay extends React.Component{
     
-    const rows = props.rows.map(row => {return (<TableRow key={row.id} data={row}/>)});
-    return (
-        <table className="table-display">
-            <thead className='table-display__thead'>
-                {/* Add here a header row */}
-                <TableRow key={'headerDataId'} data={headerRowData}/>
-            </thead>
-            <tbody>
-                {rows}
-            </tbody>
-            <tfoot>
-                {/* Add here an empty row to edit */}
-            </tfoot>
-        </table>
-    );
+
+    constructor (props) {
+        super(props);
+        // Temporary placeholder for data from db
+        this.resRows = [
+            {
+                id: 1,
+                nr_laboranta: 2137,
+                ilosc: 1,
+                miejsce: 102,
+                nazwa: 'Ławka',
+                nr_inwentarzowy: 2115,
+                uzytkownik: 'Tamul',
+                rodzaj: 'Mebel',
+                typ: true,
+                wybrakowanie: false
+            },
+            {
+                id: 2,
+                nr_laboranta: 2137,
+                ilosc: 1,
+                miejsce: 102,
+                nazwa: 'Ławka',
+                nr_inwentarzowy: 2115,
+                uzytkownik: 'Tamul',
+                rodzaj: 'Mebel',
+                typ: false,
+                wybrakowanie: false
+            },
+            {
+                id: 3,
+                nr_laboranta: 2137,
+                ilosc: 1,
+                miejsce: 102,
+                nazwa: 'Ławka',
+                nr_inwentarzowy: 2115,
+                uzytkownik: 'Tamul',
+                rodzaj: 'Mebel',
+                typ: true,
+                wybrakowanie: true
+            }
+        ];
+        console.log(this.props.link)
+    }
+
+
+    render() {
+        console.log('TableRerender');
+        // creates list of <TableRow/> containing data from props.rows
+        this.rows = this.resRows.map(row => {return (<TableRow key={row.id} data={row}/>)});
+
+        return (
+            <table className="table-display">
+                <thead className='table-display__thead'>
+                    {/* Header of data */}
+                    <tr>
+                        <td className='table-data'>Id</td>
+                        <td className='table-data'>Numer Laboranta</td>
+                        <td className='table-data'>Ilość</td>
+                        <td className='table-data'>Miejsce</td>
+                        <td className='table-data'>Nazwa</td>
+                        <td className='table-data'>Numer Inwentarzowy</td>
+                        <td className='table-data'>Użytkownik</td>
+                        <td className='table-data'>Rodzaj</td>
+                        <td className='table-data'>Typ</td>
+                        <td className='table-data'>Wybrakowanie</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* Rows of data */}
+                    {this.rows}
+                </tbody>
+                <tfoot>
+                    {/* Add here an empty row to edit */}
+                </tfoot>
+            </table>
+        );
+    }
+    
 };
 
 export default TableDisplay;
--- client/src/components/TableRow.js
+++ client/src/components/TableRow.js
@@ -1,20 +1,97 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 class TableRow extends React.Component{
-
     constructor (props) {
         super(props);
-        this.columns = [];
-        for(let item in props.data){
-            this.columns.push(props.data[item]);
-        }
-        this.columns = this.columns.map(data => {return(<td className='table-data'>{data}</td>)})
+        this.data = this.props.data;
+        this.state = {
+            inputValue: null
+        };
+        this.handleInputChange = this.handleInputChange.bind(this);
+
+        // fill these variables with data from DB
+        this.nr_laborantow = [1,2,3,4];
+        this.miejsca = [303,111,2];
+        this.uzytkownicy = ['Adam', "Marek", "Heniek"];
+        this.rodzaje = ['Komputery', 'Stoły', 'Krzesła'];
     }
 
+    handleInputChange(event) {
+        this.setState({
+            inputValue: event.target.value
+        });
+        
+        console.log(this.state, event.target.value, event.target.name)
+        this.data[event.target.name] = event.target.value;
+
+        // Here you can make db update query
+        // place for query
+    }    
+
     render () {
+        // fetch here nr_laborantow, miejsca, uzytkownicy, rodzaje with data from db
+        const nr_laboranta_fields = this.nr_laborantow.map(data => {return(<option value={data}>{data}</option>)});
+        const miejsce_fields = this.miejsca.map(data => {return(<option value={data}>{data}</option>)});
+        const uzytkownik_fields = this.uzytkownicy.map(data => {return(<option value={data}>{data}</option>)});
+        const rodzaj_fields = this.rodzaje.map(data => {return(<option value={data}>{data}</option>)});
+
         return (
             <tr className="table-row">
-                {this.columns}
+                {/* unchangable id field */}
+                <td className='table-data'>{this.data.id}</td>
+
+                {/* selectable number field */}
+                <td className='table-data'>   
+                    <select name={'nr_laboranta'} onChange={this.handleInputChange} defaultValue={this.data.nr_laboranta}>
+                        {nr_laboranta_fields}
+                    </select>
+                </td>
+
+                {/* number field */}
+                <td className='table-data'><input type='number' name={"ilosc"} onChange={this.handleInputChange} value={this.data.ilosc}/></td>
+
+                {/* selectable number field */}
+                <td className='table-data'>   
+                    <select name={'miejsce'} onChange={this.handleInputChange} defaultValue={this.data.miejsce}>
+                        {miejsce_fields}
+                    </select>
+                </td>
+
+                {/* text field */}
+                <td className='table-data'><input type="text"   name={'nazwa'} onChange={this.handleInputChange} value={this.data.nazwa}/></td>
+
+                {/* number field */}
+                <td className='table-data'><input type="number" name={'nr_inwentarzowy'} onChange={this.handleInputChange} value={this.data.nr_inwentarzowy}/></td>
+
+                {/* selectable text field */}
+                <td className='table-data'>   
+                    <select name={'uzytkownik'} onChange={this.handleInputChange} defaultValue={this.data.uzytkownik}>
+                        {uzytkownik_fields}
+                    </select>
+                </td>
+
+                {/* selectable text field */}
+                <td className='table-data'>   
+                    <select name={'rodzaj'} onChange={this.handleInputChange} defaultValue={this.data.rodzaj}>
+                        {rodzaj_fields}
+                    </select>
+                </td>
+
+                {/* selectable bool field */}
+                <td className='table-data'>
+                    <select name={'typ'} onChange={this.handleInputChange} defaultValue={this.data.typ}>
+                        <option value={true}>Stanowy</option>
+                        <option value={false}>Bezstanowy</option>
+                    </select>
+                </td>
+
+                {/* selectable bool field */}
+                <td className='table-data'>
+                    <select name={'wybrakowanie'} onChange={this.handleInputChange} defaultValue={this.data.wybrakowanie}>
+                        <option value={true}>Tak</option>
+                        <option value={false}>Nie</option>
+                    </select>
+                </td>
             </tr>
         );
     }
